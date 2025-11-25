2240839df5a7d25c11761664bbe8a0363615cad2
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-25 11:18:46 +0545

    feat: Completed Task 1 of Week 5

---
+++ Week5/Task1/BankAccount.cs
@@ -0,0 +1,55 @@
+﻿namespace Task1;
+
+public class BankAccount(long accountNumber, double balance)
+{
+    private double balance = balance;
+
+    public long AccountNumber { get; }
+
+    public double Balance
+    {
+        get => balance;
+        private set
+        {
+            if (value < 0)
+            {
+                throw  new ArgumentOutOfRangeException(nameof(value));
+            }
+            balance = value;
+        }
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Deposit Amount Cannot Be Zero or Negative");
+            return;
+        }
+
+        balance += amount;
+        Console.WriteLine($"Deposit Amount: {amount}");
+        Console.WriteLine($"New Account Balance: {balance}");
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("Withdraw Amount Cannot Be Zero or Negative");
+            return;
+        }
+
+        if (amount > balance)
+        {
+            Console.WriteLine("Insufficient Balance in the Account");
+            Console.WriteLine($"Current Account Balance: {balance}");
+            return;
+        }
+        
+        balance -= amount;
+        Console.WriteLine("Amount Withdrawal Successful");
+        Console.WriteLine($"Withdraw Amount: {amount}");
+        Console.WriteLine($"New Account Balance: {balance}");
+    }
+}
---
+++ Week5/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount bankaccount = new(20251125, 1000);
+        Console.WriteLine($"Bank Account Number: {bankaccount.AccountNumber}");
+        
+        bankaccount.Deposit(10000);
+        bankaccount.Withdraw(5600);
+        
+        Console.WriteLine($"Remaining Balance: {bankaccount.Balance}");
+    }
+}
