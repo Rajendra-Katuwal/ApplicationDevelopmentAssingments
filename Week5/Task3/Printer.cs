3ffa9744d8afb8c6d19db99814be14465ac505a5
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-26 08:25:05 +0545

    feat: completed Task3 Week5

---
+++ Week5/Task3/Printer.cs
@@ -0,0 +1,23 @@
+﻿namespace Task3;
+
+public class Printer
+{
+    public void Print(string message)
+    {
+        Console.WriteLine($"Printing String: {message}");
+    }
+
+    public void Print(int number)
+    {
+        Console.WriteLine($"Printing Number: {number}");
+    }
+
+    public void Print(string message, int count)
+    {
+        for (var i = 0; i < count; i++)
+        {
+            Console.WriteLine($"Count {i}: {message}");
+        }
+    }
+
+}
---
+++ Week5/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Task3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Printer printer = new Printer();
+        printer.Print("Hello World!");
+        printer.Print(3);
+        printer.Print("Bye World!", 5);
+    }
+}
