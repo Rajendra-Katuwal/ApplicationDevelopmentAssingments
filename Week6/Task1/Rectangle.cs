a2d0eed16979b71af5b365e47b6f72a1d9fb9e1c
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-12-02 11:10:47 +0545

    feat: completed task1 week6 workshop

---
+++ Week6/Task1/Rectangle.cs
@@ -0,0 +1,23 @@
+﻿namespace Task1;
+
+public class Rectangle
+{
+    private double length;
+    private double breadth;
+
+    public double Length
+    {
+        get => length;
+        set => length = value;
+    }
+
+    public double Breadth
+    {
+        get => breadth;
+        set => breadth = value;
+    }
+    
+    public double GetArea() =>  length * breadth;
+    public string ShowDetails() => $"Length: {length}, Breadth: {breadth}";
+    public double CalculatePerimeter() => 2 * (length + breadth);
+}
