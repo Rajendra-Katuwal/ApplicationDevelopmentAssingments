941889fd4649ad113d6d68e283acebdf4c178922
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-12-02 11:55:47 +0545

    feat: completed task3 of week6 workshop

---
+++ Week6/Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Task3;
+
+class Program
+{
+    public static void ProcessNumbers(int[] numbers, Func<int, bool> condition)
+    {
+        foreach (var num in numbers)
+        {
+            if (condition(num))
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Even Numbers");
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 20, 44, 22 };
+        ProcessNumbers(arr, n => n%2==0 );
+        Console.WriteLine("");
+        
+        Console.WriteLine("Numbers greater than 10");
+        ProcessNumbers(arr, n => n>10);
+    }
+}
